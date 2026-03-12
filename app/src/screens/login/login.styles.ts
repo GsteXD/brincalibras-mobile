5c48f1d9ea17e9818499a89bfe584743743c4f6a
Gustavo Tsuru <g.tsuruendo@gmail.com>
2026-03-12 17:53:21 -0300

    style: Estilização do modal

--- app/src/screens/login/login.styles.ts
+++ app/src/screens/login/login.styles.ts
@@ -1,6 +1,7 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  // Formatação geral da página
   container: { flex: 1, backgroundColor: '#fff', padding: 40, justifyContent: 'center' },
   title: { fontSize: 32, fontWeight: 'bold', textAlign: 'center', marginBottom: 30 },
   socialContainer: { borderWidth: 1, borderColor: '#ddd', borderRadius: 15, padding: 20, alignItems: 'center' },
@@ -19,8 +20,18 @@ export const styles = StyleSheet.create({
   footerText: { textAlign: 'center', marginTop: 15, color: '#333' },
   link: { color: '#00aeef', fontWeight: 'bold', textDecorationLine: 'underline' },
   customPlaceholder: { position: 'absolute', left: 45, color: '#aaa', zIndex: -1},
+  
+  // Caixas e diálogos de erro
   errorBox: { backgroundColor: '#fcc5c5', borderWidth: 2, borderColor: '#ff3232', padding: 10, marginBottom: 20, alignItems: 'center', justifyContent: 'center' },
   errorText: { color: '#d32f2f', fontWeight: 'bold', textAlign: 'center' },
   msgErro: { color: 'red', fontSize: 12, marginBottom: 6, marginLeft: 10 },
-  labelErro: { flexDirection: 'row', alignItems: 'flex-end' }
+  labelErro: { flexDirection: 'row', alignItems: 'flex-end' },
+  
+  // Modal
+  modalOverlay: { flex: 1, backgroundColor: 'rgba(0,0,0,0.5)', justifyContent: 'center', alignItems: 'center' },
+  successCard: { width: '80%', backgroundColor: '#fff', borderRadius: 20, padding: 25, alignItems: 'center', elevation: 10 /*Sombra no Android */, shadowColor: '#000', /*Sombra no IOS */ shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 4 },
+  successTitle: { fontSize: 22, fontWeight: 'bold',marginTop: 15, color: '#333' },
+  successMessage: { fontSize: 16, color: '#666', textAlign: 'center', marginVertical: 15 },
+  successButton: { backgroundColor: '#000', paddingVertical: 12, paddingHorizontal: 40, borderRadius: 10, marginTop: 10 },
+  successButtonText: { color: '#fff', fontSize: 16, fontWeight: 'bold' },
 });
